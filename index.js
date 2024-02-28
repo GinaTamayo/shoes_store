989c8b0a13dec3ac59a6b22f0d42f5b2d9458758
Gina Tamayo <ginatamayo1618@gmail.com>
2024-02-27 22:35:35 -0500

    creacion de archivos json

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
-let shoes = [
-    {id: "producto-01", name: "tennis nice", price: 62.000, category: "deportivo", image:  "../images/mangas/vinlandSaga01.webp", description: ""},
-]
 
 
-const flagsElement = document.getElementById('flags')
-flagsElement.addEventListener('click', (e)=> {
-    
+const container = document.getElementById('container-card');
+
+function loadLanguage(lang) {
+    fetch('languages/' + lang + '.json')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('store-title').textContent = data.title;
+
+        container.innerHTML = '';
+        createCards(data);
+
+        const idiomActuall = document.getElementById('language');
+const listIdioms = document.getElementById('languages');
+const idioms = document.querySelectorAll('.option');
+
+idiomActuall.addEventListener('click', () => {
+    listIdioms.classList.toggle('toggle');
 })
+        
+    });
+}
+
+
+function createCards(data) {
+    data.products.forEach(product => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const containerImg = document.createElement('div');
+        containerImg.classList.add('container-image');
+
+        const img = document.createElement('img');
+        img.src = product.image;
+        img.alt = 'Product Image';
+
+        const name = document.createElement('h2');
+        name.textContent = product.name;
+
+        const price = document.createElement('p');
+        price.textContent = product.price;
+
+        const button = document.createElement('button');
+        button.textContent = product.button;
+
+        containerImg.appendChild(img);
+        card.appendChild(containerImg);
+        card.appendChild(name);
+        card.appendChild(price);
+        card.appendChild(button);
+
+        container.appendChild(card);
+    });
+}
